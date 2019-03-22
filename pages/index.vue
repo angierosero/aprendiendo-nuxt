cb0d2413ba794896a2c1fcc91e345f617f42d599
angie.rosero <angieroseroa@gmail.com>
2019-03-21 20:26:40 -0500

    Iniciando proyecto

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Hola {{msg}}</h1>
+    <input type="text" v-model="msg">
+    <button @click="guardar">Guardar</button>
+    <table>
+        <tr>
+            <th>Nombre</th>
+        </tr>
+        <tr v-for="{item, index} in municipio" :key="index">
+            <td> {{ item }}</td>
+        </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "Angie rosero",
+      municipio: ['Mocoa','Villagarzon','puerto Asis']
+    };
+  },
+  methods: {
+      guardar(){
+        this.municipio.push(this.msg)
+        this.msg=''
+      }
+  }
+};
+</script>
+
